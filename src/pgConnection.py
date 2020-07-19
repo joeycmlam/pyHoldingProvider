8e52f56b5a32d353d2463f9ed546d11332787ef6
joeylam <joey.cm.lam@gmail.com>
2020-07-19 15:08:01 +0800

    web services

--- src/pgConnection.py
+++ src/pgConnection.py
@@ -1,15 +1,16 @@
 import psycopg2
 
-def showdata():
-    conn = psycopg2.connect("dbname=mydb user=postgres password=qwerty02")
+def getHolding():
+    conn = psycopg2.connect("dbname=mydb user=postgres password=xxxxx")
     cur = conn.cursor()
-    cur.execute('select stock_code, quantity from holding')
+    cur.execute('select stock_code, quantity, bookcost from holding')
     holdings = cur.fetchall()
 
     for row in holdings:
         print('stock code = ', row[0])
         print('quantity = ', row[1])
+        print('bookcost = ', row[2])
 
 if __name__ == '__main__':
 
-    showdata()
+    getHolding()
